edf99a0f7a1750af390201ecc41666eb9cfe1866
vovangrk <vovangrk@ukr.net>
2026-04-05 18:29:36 +0300

    hw 4

--- js/task-3.js
+++ js/task-3.js
@@ -1,17 +1,22 @@
-function filterArray(numbers, value) {
-    const a = [];
-    for (const items of numbers) {
-        if (items > value) {
-            a.push(items);
-        }
+const profile = {
+    username: "Jacob",
+    playTime: 300,
+    changeUsername(newName) {
+       return this.username = newName;
+        
+    },
+    updatePlayTime(hours) {
+       return this.playTime += hours;
+    },
+    getInfo() {
+        return `${this.username} has ${this.playTime} hours!`;
     }
-    return a;
+};
 
-    
-}
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
 
-console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
-console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
-console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
-console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
-console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
+profile.changeUsername("Marco");
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
